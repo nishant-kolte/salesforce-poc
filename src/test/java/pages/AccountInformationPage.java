08eef5bf45e1ef1afb42485d698f1fcb9bf286d9
Shishant Yadav <shishant.yadav@neutrinotechlabs.com>
2023-03-29 16:53:50 +0530

    Adding Dob

--- src/test/java/pages/AccountInformationPage.java
+++ src/test/java/pages/AccountInformationPage.java
@@ -14,12 +14,13 @@ import utilities.CommonUtils;
 import static utilities.CommonUtils.actionObject;
 import static utilities.CommonUtils.element;
 public class AccountInformationPage {
-    private By nextButton= By.xpath("//button[@name = 'next']");
-    private By firstNameField= By.xpath("//input[@name='PatientConnect__PC_First_Name__c']");
-    private By lastNameField= By.xpath("//input[@name='PatientConnect__PC_Last_Name__c']");
-    private By insuranceStatusField= By.xpath("//button[@name='PHX_Insurance_Status__c']");
-    private By saveRecordButton= By.xpath("//button[@name='saveRecord']");
-    private By successMessage= By.xpath("//div[@data-key='success']/div/div/div/span");
+    private By nextButton = By.xpath("//button[@name = 'next']");
+    private By firstNameField = By.xpath("//input[@name='PatientConnect__PC_First_Name__c']");
+    private By lastNameField = By.xpath("//input[@name='PatientConnect__PC_Last_Name__c']");
+    private By insuranceStatusField = By.xpath("//button[@name='PHX_Insurance_Status__c']");
+    private By saveRecordButton = By.xpath("//button[@name='saveRecord']");
+    private By successMessage = By.xpath("//div[@data-key='success']/div/div/div/span");
+    private By dob = By.xpath("//input[@name='PatientConnect__PC_Date_of_Birth__c']");
 
 //    @FindBy(xpath="//button[@name='Delete']")
 //    public static WebElement deleteAccountButton;
@@ -30,49 +31,88 @@ public class AccountInformationPage {
 
 
     @Step("click on Next button")
-    public void clickNextButton(){
-            AppUtils.waitForElementPresent(element(nextButton));
-            element(nextButton).click();
-            CommonUtils.logStepAsPassedInExtentReport(String.format("click on Next button"));
-        }
+    public void clickNextButton() {
+        AppUtils.waitForElementPresent(element(nextButton));
+        element(nextButton).click();
+        CommonUtils.logStepAsPassedInExtentReport(String.format("click on Next button"));
+    }
+
     @Step("enter user data")
-    public void enterUserData(){
+    public void enterUserData() {
         element(firstNameField).sendKeys("Abc");
         element(lastNameField).sendKeys("Xyz");
+        element(dob).sendKeys(enterDOB("March",4,2001));
         //Actions actions = new Actions(driver);
-        actionObject().click(element(insuranceStatusField)).sendKeys(Keys.ARROW_DOWN,Keys.ARROW_DOWN,Keys.ENTER).build().perform();
+        actionObject().click(element(insuranceStatusField)).sendKeys(Keys.ARROW_DOWN, Keys.ARROW_DOWN, Keys.ENTER).build().perform();
         CommonUtils.logStepAsPassedInExtentReport(String.format("enter user data"));
-        }
+    }
+
     @Step("click Save Button")
-    public void clickSaveButton(){
+    public void clickSaveButton() {
         element(saveRecordButton).click();
         CommonUtils.logStepAsPassedInExtentReport(String.format("click Save Button"));
-        }
+    }
+
     @Step("verify success message once record is created created")
-    public void verifySuccessMessage(){
+    public void verifySuccessMessage() {
         AppUtils.waitForElementPresent(element(successMessage));
         //waitForElementLoad(successMessage,"60");
         System.out.println(element(successMessage).getText());
         Assert.assertEquals(element(successMessage).getText(), "Record was saved.");
         CommonUtils.logStepAsPassedInExtentReport(String.format("verify success message once record is created created"));
+    }
+
+    /*
+        public void deleteAccount(){
+            waitForElementLoad(deleteAccountButton,"60");
+            deleteAccountButton.click();
+            waitForElementClickable(deleteButtonOnDialogBox,"60");
+            Actions actions = new Actions(driver);
+            actions.moveToElement(deleteButtonOnDialogBox).click().build().perform();
+            //deleteButtonOnDialogBox.click();
         }
-/*
-    public void deleteAccount(){
-        waitForElementLoad(deleteAccountButton,"60");
-        deleteAccountButton.click();
-        waitForElementClickable(deleteButtonOnDialogBox,"60");
-        Actions actions = new Actions(driver);
-        actions.moveToElement(deleteButtonOnDialogBox).click().build().perform();
-        //deleteButtonOnDialogBox.click();
-    }
-    public void verifyDeleteMessage(){
-        waitForElementLoad(deleteAccountMessage,"60");
-        System.out.println(deleteAccountMessage.getText());
-        Assert.assertEquals(deleteAccountMessage.getText(),"Account \"Abc Xyz\" was deleted. ");
-    }
-*/
+        public void verifyDeleteMessage(){
+            waitForElementLoad(deleteAccountMessage,"60");
+            System.out.println(deleteAccountMessage.getText());
+            Assert.assertEquals(deleteAccountMessage.getText(),"Account \"Abc Xyz\" was deleted. ");
+        }
+    */
+    public String enterDOB(String month, int day, int year) {
+        String mon = "";
+        switch (month) {
+
+            case "January": mon = "Jan";
+            break;
+            case "February": mon = "Feb";
+            break;
+            case "March": mon = "Mar";
+            break;
+            case "April": mon = "Apr";
+            break;
+            case "May": mon = "May";
+            break;
+            case "June": mon = "Jun";
+            break;
+            case "July": mon = "Jul";
+            break;
+            case "August": mon = "Aug";
+            break;
+            case "September": mon = "Sep";
+            break;
+            case "October": mon = "Oct";
+            break;
+            case "November": mon = "Nov";
+            break;
+            case "December": mon = "Dec";
+            break;
+            default : System.out.println("Invalid Month!");
+
+        }
+        return mon+" "+day+", "+year;
 
 
+
     }
+}
 
 
